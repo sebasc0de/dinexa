4b309c1f842117a6272c460c656c50516fdf99d6
sebasc0de <nahuelsebastiandiaz@icloud.com>
2023-04-13 19:19:23 -0300

    Enviroment variables

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://ckdhmjpurmkwwvmssgwk.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrZGhtanB1cm1rd3d2bXNzZ3drIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODExNTUwODgsImV4cCI6MTk5NjczMTA4OH0.EBLn1KDhJMMNjrT8Kwp6sz-_tr8esWl9w3YUvTd3r7Q"
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_KEY as string
 );
