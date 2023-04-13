fab3c831b696d06b54276eac6c6ede84f97c05c2
sebasc0de <nahuelsebastiandiaz@icloud.com>
2023-04-13 18:44:01 -0300

    Toast notifications added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,19 @@
 import "@/styles/styles.scss";
+import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import type { AppProps } from "next/app";
 
 // Project imports
-import store, { persistor } from "../store";
-import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
 import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import store, { persistor } from "../store";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <PersistGate loading={<p>Loading...</p>} persistor={persistor}>
+        <ToastContainer />
         <Component {...pageProps} />
       </PersistGate>
     </Provider>
