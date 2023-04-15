4fd23bd75a625dc58cd81cb1591235fc807fdd1f
sebasc0de <nahuelsebastiandiaz@icloud.com>
2023-04-15 12:48:18 -0300

    Carrousel added to hero sectio none

--- src/components/UI/HeroSections/HeroSectionOne.tsx
+++ src/components/UI/HeroSections/HeroSectionOne.tsx
@@ -1,11 +1,11 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Carousel, Col, Container, Row } from "react-bootstrap";
 import { MegaTitleTwo, MegaTitleTwoProps } from "../Titles/MegaTitleTwo";
 
 export const HeroSectionOne = ({ data }: { data: HeroSectionOneProps }) => {
   return (
-    <Container>
+    <Container className="heroSectionOne">
       <Row>
-        <Col lg={7} className="p-5">
+        <Col lg={7}>
           <MegaTitleTwo
             title={data.title}
             gradientText={data.gradientText}
@@ -13,10 +13,47 @@ export const HeroSectionOne = ({ data }: { data: HeroSectionOneProps }) => {
             Component={data.Component}
           />
         </Col>
-        <Col lg={5}>b</Col>
+        <Col lg={5}>
+          <ImagesCollectionCarrousel />
+        </Col>
       </Row>
     </Container>
   );
 };
+
+function ImagesCollectionCarrousel() {
+  return (
+    <Carousel
+      fade
+      indicators={false}
+      controls={false}
+      interval={2500}
+      className="carrousel"
+      variant="dark"
+    >
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/images/books.jpg"
+          alt="First slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="images/controller.jpg"
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="images/sun_glasses.jpg"
+          alt="Third slide"
+        />
+      </Carousel.Item>
+    </Carousel>
+  );
+}
 
 interface HeroSectionOneProps extends MegaTitleTwoProps {}
