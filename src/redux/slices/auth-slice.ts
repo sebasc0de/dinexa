c7f71a0502f11038861fed4c26609843ece1c503
sebasc0de <nahuelsebastiandiaz@icloud.com>
2023-04-21 19:18:16 -0300

    Auth slice changed

--- src/redux/slices/auth-slice.ts
+++ src/redux/slices/auth-slice.ts
@@ -3,12 +3,10 @@ import { User } from "../../types";
 
 interface State {
   user: undefined | User;
-  autenthicated: boolean;
 }
 
 const initialState: State = {
   user: undefined,
-  autenthicated: false,
 };
 
 export const authSlice = createSlice({
@@ -16,13 +14,11 @@ export const authSlice = createSlice({
   initialState,
   reducers: {
     setUserSession(state, action: PayloadAction<User>) {
-      state.autenthicated = true;
       state.user = action.payload;
     },
 
     logOutUser(state) {
       state.user = undefined;
-      state.autenthicated = false;
     },
   },
 });
