84f05b737f2f61e3552aadb866e8e49972215f5a
sebasc0de <nahuelsebastiandiaz@icloud.com>
2023-04-24 15:47:30 -0300

    Actives styles added to dashboard sidebar

--- src/components/UI/Sidebars/SidebarOne.tsx
+++ src/components/UI/Sidebars/SidebarOne.tsx
@@ -1,18 +1,21 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { Nav } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 
 const SidebarOne = ({ navData }: Props) => {
+  const route = useRouter().route;
   const { t } = useTranslation(["sidebars"]);
 
   return (
-    <div className="sidebarOne">
+    <div className="sidebarOne d-none d-md-block">
       <Nav defaultActiveKey="/dashboard" className="flex-column gap-5 p-4">
         {navData.map((item, i) => {
+          const NAV_ACTIVE = item.route === route && "active";
           const Icon = item.Icon;
 
           return (
-            <Nav.Link className="nav-link">
+            <Nav.Link className={`nav-link ${NAV_ACTIVE}`}>
               <Icon />
               <Link href={item.route}>{t(`dashboard.${item.text}`)}</Link>
             </Nav.Link>
