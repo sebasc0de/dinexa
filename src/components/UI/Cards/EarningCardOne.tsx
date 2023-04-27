b506248aaedd62a863ee87f84a8b2dd697caa7c3
sebasc0de <nahuelsebastiandiaz@icloud.com>
2023-04-27 19:08:03 -0300

    Wallet page earning cards icons

--- src/components/UI/Cards/EarningCardOne.tsx
+++ src/components/UI/Cards/EarningCardOne.tsx
@@ -1,7 +1,7 @@
-const EarningCardOne = ({ title, earning }: Props) => {
+const EarningCardOne = ({ title, earning, Icon }: Props) => {
   return (
     <div className="earningCardOne">
-      <span>i</span>
+      <Icon size={40} />
       <div>
         <span className="pre-title">{title}</span>
         <h4 className="earning">${earning.toFixed(2)}</h4>
@@ -13,6 +13,11 @@ const EarningCardOne = ({ title, earning }: Props) => {
 interface Props {
   title: string;
   earning: number;
+  Icon: React.FC<Icon>;
+}
+
+interface Icon {
+  size: number;
 }
 
 export default EarningCardOne;
--- src/pages/wallet.tsx
+++ src/pages/wallet.tsx
@@ -1,29 +1,34 @@
 import { Col, Row } from "react-bootstrap";
-import Layout from "../components/layouts/Dashboard";
 
+// Components
 import Form from "../components/wallet/Form";
 import Calculator from "../components/wallet/SavingCalculator";
 import EarningCardOne from "../components/UI/Cards/EarningCardOne";
+import Layout from "../components/layouts/Dashboard";
+
+// Icons
+import { TbMoneybag, TbPigMoney } from "react-icons/tb";
+import { IoWalletOutline } from "react-icons/io5";
 
 const Wallet = () => {
   return (
     <Layout>
       <Row className="gap-3">
         <Col className="box-1">
-          <EarningCardOne earning={2} title="Total savings" />
+          <EarningCardOne Icon={TbMoneybag} earning={2} title="Total savings" />
         </Col>
         <Col className="box-1">
-          <EarningCardOne earning={2} title="Wallet" />
+          <EarningCardOne Icon={IoWalletOutline} earning={2} title="Wallet" />
         </Col>
         <Col className="box-1">
-          <EarningCardOne earning={2} title="Spended today" />
+          <EarningCardOne Icon={TbPigMoney} earning={2} title="Spended today" />
         </Col>
       </Row>
       <Row className="mt-3 gap-3">
         <Col className="box-1 p-4">
           <Form />
         </Col>
-        <Col className="box-1 p-4">
+        <Col className="box-1 p-4" style={{ height: "fit-content" }}>
           <Calculator />
         </Col>
       </Row>
