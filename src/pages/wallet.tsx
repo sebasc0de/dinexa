f7424cbeb16d7f97930402546ff766f872ee9584
sebasc0de <nahuelsebastiandiaz@icloud.com>
2023-04-27 16:20:06 -0300

    Wallet page columns styles

--- src/pages/wallet.tsx
+++ src/pages/wallet.tsx
@@ -1,8 +1,27 @@
 import { Col, Row } from "react-bootstrap";
 import Layout from "../components/layouts/Dashboard";
 
+import Form from "../components/wallet/Form";
+import Resume from "../components/wallet/SavingStylesResume";
+
 const Wallet = () => {
-  return <Layout>dsa</Layout>;
+  return (
+    <Layout>
+      <Row>
+        <Col>a</Col>
+        <Col>b</Col>
+        <Col>c</Col>
+      </Row>
+      <Row className="mt-5 gap-3">
+        <Col className="box-1 p-4">
+          <Resume />
+        </Col>
+        <Col md={5} className="box-1 p-4">
+          <Form />
+        </Col>
+      </Row>
+    </Layout>
+  );
 };
 
 export default Wallet;
