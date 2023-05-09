dc283b18463d48b3d6a31800e5e81c4be84a6dc8
sebasc0de <nahuelsebastiandiaz@icloud.com>
2023-05-09 16:42:39 -0300

    Localstorage on wallet removed

--- src/store/index.ts
+++ src/store/index.ts
@@ -8,7 +8,7 @@ const persistConfig = {
   version: 1,
   storage,
   blacklist: [""],
-  whitelist: ["auth", "wallet"],
+  whitelist: ["auth"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
