40e637e416cf7dff6ebbabd91ac8d0a751cffe12
sebasc0de <nahuelsebastiandiaz@icloud.com>
2023-05-09 15:05:34 -0300

    Interface error fixed on auth domain layer

--- src/modules/auth/domain/ensurePasswordIsValid.ts
+++ src/modules/auth/domain/ensurePasswordIsValid.ts
@@ -1,5 +1,10 @@
 import { ValidationResult } from "../../../types";
 
+const validResponse = {
+  msg: "",
+  isValid: true,
+};
+
 export const ensurePasswordIsValid = (
   password: string,
   confirmPassword: string
@@ -12,22 +17,25 @@ export const ensurePasswordIsValid = (
   const matchResult = checkPasswordMatch(password, confirmPassword);
   if (!matchResult.isValid) return matchResult;
 
-  return { isValid: true, message: "" };
+  return validResponse;
 };
 
 const checkPasswordLength = (password: string): ValidationResult => {
   if (password.length < 8)
     return {
       isValid: false,
-      message: "The password must be at least 8 characters long.",
+      msg: "The password must be at least 8 characters long.",
     };
 
-  return { isValid: true, message: "" };
+  return validResponse;
 };
 
-const checkPasswordMatch = (password: string, confirmPassword: string) => {
+const checkPasswordMatch = (
+  password: string,
+  confirmPassword: string
+): ValidationResult => {
   if (password !== confirmPassword)
-    return { isValid: false, message: "Passwords do not match" };
+    return { isValid: false, msg: "Passwords do not match" };
 
-  return { isValid: true, message: "" };
+  return validResponse;
 };
